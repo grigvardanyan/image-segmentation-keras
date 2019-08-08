73a47222424b9bfef019773230c176e5652b4138
Grigor <grigor.vardanyan@helpsystems.com>
2019-08-08 15:32:46 +0400

    Changed segmentation input from rgb to binary masks

--- keras_segmentation/data_utils/data_loader.py
+++ keras_segmentation/data_utils/data_loader.py
@@ -5,6 +5,7 @@ import glob
 import itertools
 import os
 from tqdm import tqdm
+import ntpath
 
 from ..models.config import IMAGE_ORDERING
 from .augmentation import augment_seg
@@ -13,27 +14,26 @@ import random
 random.seed(0)
 class_colors = [  ( random.randint(0,255),random.randint(0,255),random.randint(0,255)   ) for _ in range(5000)  ]
 
-
-
-
 def get_pairs_from_paths( images_path , segs_path ):
 	images = glob.glob( os.path.join(images_path,"*.jpg")  ) + glob.glob( os.path.join(images_path,"*.png")  ) +  glob.glob( os.path.join(images_path,"*.jpeg")  )
 	segmentations  =  glob.glob( os.path.join(segs_path,"*.png")  ) 
 
-	segmentations_d = dict( zip(segmentations,segmentations ))
+    input_output = dict()
+    n_classes = 18
+    lables = ["background","skin","nose","eye_g","l_eye","r_eye","l_brow","r_brow","l_ear","r_ear","mouth","u_lip","l_lip","hair","hat","ear_r","neck_l","neck","cloth"]
 
-	ret = []
+    for seg_img in segmentations:
+        id = seg_img.split("_")[0]
+        assert(seg_img == id), ("Current file doesn't has proper file format it need to has following format index_mask: " + seg_img)
 
-	for im in images:
-		seg_bnme = os.path.basename(im).replace(".jpg" , ".png").replace(".jpeg" , ".png")
-		seg = os.path.join( segs_path , seg_bnme  )
-		assert ( seg in segmentations_d ),  (im + " is present in "+images_path +" but "+seg_bnme+" is not found in "+segs_path + " . Make sure annotation image are in .png"  )
-		ret.append((im , seg) )
+        id = int(id)
+        if id not in input_output:
+            input_output[id] = [""] * n_classes
 
-	return ret
-
-
-
+        for i in range(len(lables)):
+            if lables[i] in seg_img:
+                input_output[id][i] = seg_img
+	return input_output,images
 
 def get_image_arr( path , width , height , imgNorm="sub_mean" , odering='channels_first' ):
 
@@ -61,36 +61,21 @@ def get_image_arr( path , width , height , imgNorm="sub_mean" , odering='channel
 		img = np.rollaxis(img, 2, 0)
 	return img
 
-
-
-
-
-
-def get_segmentation_arr( path , nClasses ,  width , height , no_reshape=False ):
+def get_segmentation_arr( paths , nClasses ,  width , height , no_reshape=False ):
 
 	seg_labels = np.zeros((  height , width  , nClasses ))
-		
-	if type( path ) is np.ndarray:
-		img = path
-	else:
-		img = cv2.imread(path, 1)
-
-	img = cv2.resize(img, ( width , height ) , interpolation=cv2.INTER_NEAREST )
-	img = img[:, : , 0]
-
-	for c in range(nClasses):
-		seg_labels[: , : , c ] = (img == c ).astype(int)
-
-
+	for i in range(len(paths)):
+        if paths[i] != "":
+	        img = cv2.imread(paths[i], 0)
+            img = cv2.resize(img, ( width , height ) , interpolation=cv2.INTER_NEAREST )
+            seg_labels[:,:,i] = (img == 255 ).astype(int)
+        
 	
 	if no_reshape:
 		return seg_labels
 
-	seg_labels = np.reshape(seg_labels, ( width*height , nClasses ))
+	seg_labels = np.reshape(seg_labels, ( width * height , nClasses ))
 	return seg_labels
-
- 
-
 
 def verify_segmentation_dataset( images_path , segs_path , n_classes ):
 	
@@ -111,18 +96,18 @@ def verify_segmentation_dataset( images_path , segs_path , n_classes ):
 def image_segmentation_generator( images_path , segs_path ,  batch_size,  n_classes , input_height , input_width , output_height , output_width  , do_augment=False ):
 	
 
-	img_seg_pairs = get_pairs_from_paths( images_path , segs_path )
+	img_id_seg_pairs , images = get_pairs_from_paths( images_path , segs_path )
 	random.shuffle( img_seg_pairs )
-	zipped = itertools.cycle( img_seg_pairs  )
+	zipped = itertools.cycle( img_id_seg_pairs  )
 
 	while True:
 		X = []
 		Y = []
 		for _ in range( batch_size) :
-			im , seg = next(zipped) 
+            key = next(zipped)
+			im , seg = get_path(images , key) , img_id_seg_pairs[key]
 
 			im = cv2.imread(im , 1 )
-			seg = cv2.imread(seg , 1 )
 
 			if do_augment:
 				img , seg[:,:,0] = augment_seg( img , seg[:,:,0] )
@@ -132,3 +117,17 @@ def image_segmentation_generator( images_path , segs_path ,  batch_size,  n_clas
 
 		yield np.array(X) , np.array(Y)
 
+def get_path(images,id):
+    for img in images:
+        img_name = ntpath.basename(img).split('.')[0]
+        if len(img_name) == get_length(id) and int(img_name) == id:
+            return img
+
+def get_length(id):
+    length = 0
+    while True:
+        id = id//10
+        length += 1
+        if id == 0:
+            break
+    return length
