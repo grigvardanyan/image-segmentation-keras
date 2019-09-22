e9e018212c71f0057b0e3190e2e0a8a0ac19715a
grigvardanyan <vardanyangogor95@gmail.com>
2019-09-22 11:13:16 +0400

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -121,7 +121,7 @@ def train( model  ,
 		print("With Validate")
 		#for ep in range( epochs ):
 		#print("Starting Epoch with Validate" , ep)
-		model.fit_generator( train_gen , steps_per_epoch  , validation_data=val_gen , epochs=epochs ,use_multiprocessing=False,callbacks=[cp_callback])
+		model.fit_generator( train_gen  , validation_data=val_gen , epochs=epochs ,use_multiprocessing=False,callbacks=[cp_callback])
 		if not checkpoints_path is None:
 			model.save_weights( checkpoints_path + "." + str( ep )  )
 			#print("saved " , checkpoints_path + ".model." + str( ep ) )
