59ceb2ffa07e98e42d9a2d6985f7178c755eafa1
grigvardanyan <vardanyangogor95@gmail.com>
2019-09-28 10:47:15 +0400

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -21,44 +21,6 @@ def find_latest_checkpoint( checkpoints_path ):
 
 		ep += 1
 
-
-
-def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
-    weight_a = alpha * (1 - y_pred) ** gamma * targets
-    weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
-    
-    return (tf.math.log1p(tf.math.exp(-tf.math.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b 
-
-def loss(y_true, y_pred):
-	alpha = 0.25
-	gamma = 2
-	y_pred = tf.clip_by_value(y_pred, K.epsilon(), 1 - K.epsilon())
-	logits = tf.math.log(y_pred / (1 - y_pred))
-	
-	loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
-
-    # or reduce_sum and/or axis=-1
-	return tf.reduce_mean(loss)
-
-def Mean_IOU(y_true, y_pred):
-    nb_classes = K.int_shape(y_pred)[-1]
-    iou = []
-    true_pixels = K.argmax(y_true, axis=-1)
-    pred_pixels = K.argmax(y_pred, axis=-1)
-    void_labels = K.equal(K.sum(y_true, axis=-1), 0)
-    for i in range(0, nb_classes): # exclude first label (background) and last label (void)
-        true_labels = K.equal(true_pixels, i) & ~void_labels
-        pred_labels = K.equal(pred_pixels, i) & ~void_labels
-        inter = tf.to_int32(true_labels & pred_labels)
-        union = tf.to_int32(true_labels | pred_labels)
-        legal_batches = K.sum(tf.to_int32(true_labels), axis=1)>0
-        ious = K.sum(inter, axis=1)/K.sum(union, axis=1)
-        iou.append(K.mean(tf.gather(ious, indices=tf.where(legal_batches)))) # returns average IoU of the same objects
-    iou = tf.stack(iou)
-    legal_labels = ~tf.debugging.is_nan(iou)
-    iou = tf.gather(iou, indices=tf.where(legal_labels))
-    return K.mean(iou)
-
 def iou_coef(y_true, y_pred):
     smooth=1
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
